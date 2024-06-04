715c4cf34be3b4f30fa70994e4bd518e5b8086ea
saiteja <mamidisrisaiteja@gmail.com>
2024-06-04 10:03:35 +0530

    removed testng dependency and report updates in testrunner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,7 +9,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
 
         features = {"src/main/features"},
-        glue = {"stepDefinition"}
+        glue = {"stepDefinition"},
+        plugin={"pretty","json:target/report.json"}
 
 
 
